da0d4307ff556733f9ffa9be12da8e54c88ae002
Cristobal <cristobal_jjg@hotmail.com>
2023-03-22 18:56:56 +0000

    Inicio de la landing-page
    Se añadió el texto general de descripción
    Se añadió el link al spline que se mueve
    Se añadieron los botones para acceder mediante register o loggeo con google

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { LoginRegisterPageComponent } from './login-register-page/login-register-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},  
+  {path: 'home', component: HomePageComponent},
+  {path: 'login_register', component: LoginRegisterPageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
