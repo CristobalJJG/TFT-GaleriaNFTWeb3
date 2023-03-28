31cd76897347a04c0e03ae0a0568454bea7741f6
Cristobal <cristobal_jjg@hotmail.com>
2023-03-28 10:56:54 +0100

    Inicio de login y Register

--- client/src/app/home-page/home-page.component.ts
+++ client/src/app/home-page/home-page.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss']
 })
-export class HomePageComponent {
+export class HomePageComponent implements OnInit {
+  // Flag para comprobar si mostrar el login/register o no
+  flag:boolean = false;
 
+  constructor(){
+
+  }
+
+  ngOnInit(){
+    this.flag = location.href.endsWith('home');    
+  }
 }
