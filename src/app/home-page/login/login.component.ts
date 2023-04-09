60f59161383e5d86ac80419ec5b74f30e414fa58
CristóbalJJG <cristobal_jjg@hotmail.com>
2023-04-09 16:26:19 +0100

    Validaciones generales del inicio de sesión

--- src/app/home-page/login/login.component.ts
+++ src/app/home-page/login/login.component.ts
@@ -5,23 +5,30 @@ import { FirestoreService } from 'src/app/services/firestore-service.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['../home-page.component.scss', '../login-register.component.scss']
+  styleUrls: ['../home-page.component.scss', 
+    '../login-register.component.scss', 
+    '../login-register-validations.scss']
 })
 export class LoginComponent {
-
-  mail: string | undefined;
-  pass: string | undefined;
 
   constructor(protected auth: AuthService, protected db: FirestoreService){
 
   }
 
   /* Para evitar "inyecciones" */
-  async onLogin(){
+  async onLogin(login: any){
+    if(login.mail.valid && login.pass.valid){
+      console.log("se manda", login.mail.value, login.pass.value);
+
+    }
+
+    //console.log("se manda", login.mail.value, login.pass.value);
+    
+    /* 
     if((this.mail && this.mail.trim() != " ".trim()) &&   // Si existe correo y no es vacío
         (this.pass && this.pass?.length >= 6 &&           // Si existe contraseña y es mayor a 6 dígitos
           this.pass.match('[A-Za-z0-9.]{6,}'))) {    // Si la contraseña tiene algún caracter no permitido
             this.auth.logInEmailPass(this.mail, this.pass)
-          }
+          } */
   }
 }
